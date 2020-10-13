68670d9e697c54782bc0e51de4a03e344b7688e4
Yanick <tyankee15@gmail.com>
2020-10-13 17:02:11 -0300

    Fix linters indentation issues

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     validates :name, presence: true
-    validates :password, length: {minimum: 6, maximum: 20}, presence: true
+    validates :password, length: { minimum: 6, maximum: 20 }, presence: true
 
     has_many :posts
     has_many :comments
