42dfbbdcddc86e2a6a9678bc47ec42509691b8a7
Yanick <tyankee15@gmail.com>
2020-10-13 15:09:47 -0300

    Add columns to the posts table

--- db/migrate/20201013180419_create_posts.rb
+++ db/migrate/20201013180419_create_posts.rb
@@ -1,6 +1,9 @@
 class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.references :user, foreign key: true
 
       t.timestamps
     end
