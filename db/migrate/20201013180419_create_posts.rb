36986d8a4fb6e977dd87cd30a8da6bd086f50bba
Yanick <tyankee15@gmail.com>
2020-10-13 15:31:41 -0300

    Migrate db for all 3 tables: users, posts and comments

--- db/migrate/20201013180419_create_posts.rb
+++ db/migrate/20201013180419_create_posts.rb
@@ -3,7 +3,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
     create_table :posts do |t|
       t.string :title
       t.text :body
-      t.references :user, foreign key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/migrate/20201013181658_create_comments.rb
+++ db/migrate/20201013181658_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration[5.2]
   def change
     create_table :comments do |t|
       t.text :comment
-      t.references :post, foreign key: true
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
